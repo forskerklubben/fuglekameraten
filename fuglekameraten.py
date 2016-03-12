377a3a22131eed6ae6816530c29a33a455ac4330
Ole Jørgen Legård <oj@forskerklubben.no>
2016-03-12 12:04:04 +0100

    update

--- fuglekameraten.py
+++ fuglekameraten.py
@@ -6,57 +6,19 @@ import datetime
 import smtplib # Import smtplib for the actual sending function
 from email.mime.text import MIMEText # Import the email modules we'll need
 from twython import Twython, TwythonError # Twitter bibliotek
+import urllib2 # used to verify internet connection
 
 # Antall sekunder uten bevegelse før vi poster ny tweet
-twitter_delay = 10
-
-# twitter setup
-f_app_key = open("./app_key.txt")
-APP_KEY = f_app_key.read()
-f_app_key.close()
-f_app_secret = open("./app_secret.txt")
-APP_SECRET = f_app_secret.read()
-f_app_secret.close()
-f_oauth_token = open("./oauth_token.txt")
-OAUTH_TOKEN = f_oauth_token.read()
-f_oauth_token.close()
-f_oauth_token_secret = open("./oauth_token_secret.txt")
-OAUTH_TOKEN_SECRET = f_oauth_token_secret.read()
-f_oauth_token_secret.close()
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-# epost setup
-f_fra = open("./fra.txt")
-fra = f_fra.read()
-f_fra.close()
-
-til = []
-tilmsg = ""
-with open("./to.txt") as f_to:
-    til = f_to.readlines()
-msg = MIMEText("Fuglekameraten har detektert bevegelse i fuglekassen! Klikk her: http://www.forskerklubben.no/fuglekamera/")
-msg['Subject'] = 'Bevegelse i fuglekassen!'
-msg['From'] = fra
-for to in til:
-    tilmsg += to + ","
-msg['To'] = tilmsg
-
-# IO setup
-led_pin = 3
-a_pin = 5
-b_pin = 7
-pir_pin = 11
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(led_pin, GPIO.OUT)
-GPIO.setup(pir_pin, GPIO.IN, GPIO.PUD_DOWN)
-
-# initialisering av variabler
-previous_state = False
-current_state = False
-timestamp = time.time()
+twitter_delay = 3600 # 1 time
 
 # Funksjoner
+def internet_on():
+    try:
+        response=urllib2.urlopen('http://216.58.209.99', timeout=5)
+        return True
+    except urllib2.URLError as err: pass
+    return False
+
 def discharge():
     GPIO.setup(a_pin, GPIO.IN)
     GPIO.setup(b_pin, GPIO.OUT)
@@ -87,8 +49,62 @@ def post_tweet():
     except TwythonError as e:
         print e
 
-while True:
+print("Startup")
+while not internet_on():
+	print("No Internet connection!")
+	time.sleep(1)
+print("We have Internet connection. Continuing ...")
 
+# twitter setup
+f_app_key = open("/data/forskerklubben/fuglekameraten/app_key.txt")
+APP_KEY = f_app_key.read()
+f_app_key.close()
+f_app_secret = open("/data/forskerklubben/fuglekameraten/app_secret.txt")
+APP_SECRET = f_app_secret.read()
+f_app_secret.close()
+f_oauth_token = open("/data/forskerklubben/fuglekameraten/oauth_token.txt")
+OAUTH_TOKEN = f_oauth_token.read()
+f_oauth_token.close()
+f_oauth_token_secret = open("/data/forskerklubben/fuglekameraten/oauth_token_secret.txt")
+OAUTH_TOKEN_SECRET = f_oauth_token_secret.read()
+f_oauth_token_secret.close()
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+print("Twitter OK")
+# epost setup
+f_fra = open("/data/forskerklubben/fuglekameraten/fra.txt")
+fra = f_fra.read()
+f_fra.close()
+
+til = []
+tilmsg = ""
+with open("/data/forskerklubben/fuglekameraten/to.txt") as f_to:
+    til = f_to.readlines()
+msg = MIMEText("Fuglekameraten har detektert bevegelse i fuglekassen! Klikk her: http://www.forskerklubben.no/fuglekamera/")
+msg['Subject'] = 'Bevegelse i fuglekassen!'
+msg['From'] = fra
+for to in til:
+    tilmsg += to + ","
+msg['To'] = tilmsg
+
+print("Email OK")
+# IO setup
+led_pin = 3
+a_pin = 5
+b_pin = 7
+pir_pin = 8
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(pir_pin, GPIO.IN, GPIO.PUD_DOWN)
+
+# initialisering av variabler
+previous_state = False
+current_state = False
+timestamp = time.time()
+
+print("Starting program")
+while True:
     # Les lys sensor
     value = analog_read()
     
@@ -102,12 +118,14 @@ while True:
     previous_state = current_state
     current_state = GPIO.input(pir_pin)
     if current_state != previous_state:
+        print(GPIO.input(pir_pin))
         if current_state == False:
             timestamp = time.time()
         else:
             motion = time.time()
             delta = motion - timestamp
             if (delta > twitter_delay):
+                #print("Tweet")
                 send_email()
                 post_tweet()
 
